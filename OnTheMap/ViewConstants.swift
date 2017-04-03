e22a9cc2bb87e9ed9c775b0b3068fce89baf95ba
SahilDhawan <sahildhawan2011@gmail.com>
2017-04-03 23:52:26 +0530

    Basic UI Implemented

---
+++ OnTheMap/ViewConstants.swift
@@ -0,0 +1,11 @@
+//
+//  ViewConstants.swift
+//  OnTheMap
+//
+//  Created by Sahil Dhawan on 03/04/17.
+//  Copyright © 2017 Sahil Dhawan. All rights reserved.
+//
+import UIKit
+import Foundation
+
+let loginBackground = UIColor.init(red: 30, green: 144, blue: 255, alpha: 1)
